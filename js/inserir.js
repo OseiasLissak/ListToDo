8413cf1ae4cf87c8f777d87534d36eed6251beb7
Oseias Lissak <oseiaslissak@hotmail.com>
2022-09-22 21:37:07 -0300

    Adicionado inserir.js

---
+++ js/inserir.js
@@ -0,0 +1,8 @@
+let tarefaInserida = document.querySelector("#tarefas");
+
+let botaoInserir = document.querySelector("#botao");
+
+botaoInserir.addEventListener ("click", function(event){
+console.log(tarefaInserida.value)
+
+})
